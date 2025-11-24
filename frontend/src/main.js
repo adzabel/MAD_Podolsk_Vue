5012da34b0521432e4860e981cfe59eb3b8483d1
MEI ZAHRA <dima@030382.ru>
2025-11-24 07:00:37 +0000

    2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,18 @@
+// frontend/src/main.js
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+// Глобальные стили дашборда
+import './styles/base.css'
+import './styles/layout.css'
+import './styles/components.css'
+import './styles/categories.css'
+import './styles/mockup.css'
+import './styles/modal.css'
+import './styles/print.css'
+import './styles/summary.css'
+import './styles/tokens.css'
+import './styles/utilities.css'
+import './styles/work.css'
 
 createApp(App).mount('#app')
